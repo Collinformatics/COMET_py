2477edfdfaa7f29afc3b688e932d4f632d54e618
Collin Andrews <pizzaTrucks@outlook.com>
2026-03-12 11:43:53 -0500

    Add files via upload

--- tables.py
+++ tables.py
@@ -1,29 +1,29 @@
-import sys
-
 from functions import pressKey
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
 # Make table
-sigfigs = 0
-data = {
+inSigFigs = 0
+inData = {
     'Substrates': ['AVLQSGFR', 'VILQSGFR', 'VILQTGFR', 'VILQSPFR',
                    'VILHSGFR', 'VIMQSGFR', 'VPLQSGFR', 'NILQSGFR'],
     '% Product': [0.761, 1.000, 0.110, 0.000, 0.222, 0.807, 0.000, 0.175],
     'Predicted': [0.628, 1.000, 0.019, 0.004, 0.054, 0.404, 0.004, 0.048],
     'Predicted Sq Root': [0.793, 1.0, 0.138, 0.063, 0.233, 0.635, 0.060, 0.221],
 }
-table = pd.DataFrame.from_dict(data)
+
 
 # Convert to %
-for col in table.columns[1:]: # Format values
-    if sigfigs == 0 or not sigfigs:
+table = pd.DataFrame.from_dict(inData)
+for col in table.columns[1:]:
+    if inSigFigs == 0 or not inSigFigs:
         table[col] = table[col] = (table[col] * 100).astype(int)
     else:
-        table[col] = (table[col] * 100).round(sigfigs)
+        table[col] = (table[col] * 100).round(inSigFigs)
 print(f'{table}\n')
 
+# Make figure
 h = (len(table.index) / 2) - 1
 w = len(table.columns) * 2
 fig, ax = plt.subplots(figsize=(w, h))
@@ -46,7 +46,6 @@ tbl.set_fontsize(12)
 for (row, col), cell in tbl.get_celld().items():
     text = cell.get_text()
     text.set_fontname('Times New Roman')
-    cell.set_height(0.05) # manually set row height
     text.set_verticalalignment('top')
 
 fig.tight_layout(pad=0.5)
