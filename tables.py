630ac53be63e53fe0ea3f59d1e63016ef8bf3ff8
Collin Andrews <179949844+Collinformatics@users.noreply.github.com>
2026-05-05 14:07:21 -0500

    Add files via upload

--- tables.py
+++ tables.py
@@ -4,6 +4,7 @@ from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from scipy.optimize import curve_fit
 from sklearn.metrics import r2_score
 import sys
 
@@ -20,51 +21,54 @@ pd.set_option('display.float_format', '{:,.3f}'.format)
     The order of the keys in inData will have the same order in the table.
 """
 
-# Make table
-inEnzyme = f'M{"ᵖʳᵒ"}'
-inEnzyme2 = f'M{"ᵖʳᵒ"}2'
+# Input: Data
+inEnzyme = f'M{"ᵖʳᵒ"}2'
+inEnzyme2 = f'M{"ᵖʳᵒ"}' # Secondary enzyme
 inSigFigs = 0
+inRoundVal = 3
+inNatLog = False
 inSubstrates = ['AVLQSGFR', 'VILQSGFR', 'VILQTGFR', 'VILQSPFR',
                 'VILHSGFR', 'VIMQSGFR', 'VPLQSGFR', 'NILQSGFR']
 inEmptyList = [0 for i in range(1, len(inSubstrates) + 1)]
 inData = {
     'Substrates': inSubstrates,
 
-    f'% Product {inEnzyme}': [0.54, 0.66, 0.34, 0.0, 0.26, 0.61, 0.0, 0.22],
+    f'% Product {inEnzyme}': [0.76, 0.82, 0.38, 0.0, 0.32, 0.66, 0.0, 0.23],
     f'Activity Z {inEnzyme}': inEmptyList,
-    f'% Product Rank {inEnzyme}': inEmptyList,
-    f'% St Dev {inEnzyme}': [0.01, 0.058, 0.025, 0.0, 0.027, 0.044, 0.0, 0.033],
-    f'Predicted {inEnzyme}': [0.572, 1.0, 0.015, 0.01, 0.038, 0.51, 0.034, 0.066],
-    f'Pred Z {inEnzyme}': inEmptyList,
-    # f'Predicted ln {inEnzyme}': inEmptyList,
+    f'Activity Rank {inEnzyme}': inEmptyList,
+    f'% St Dev {inEnzyme}': [0.1, 0.09, 0.02, 0, 0.06, 0.09, 0, 0.05],
+    f'Predicted {inEnzyme}': [0.976, 1.0, 0.035, 0.005, 0.053, 0.362, 0.006, 0.01],
+    f'Predicted Z {inEnzyme}': inEmptyList,
     f'Predicted Rank {inEnzyme}': inEmptyList,
 
-    f'% Product {inEnzyme2}':      [0.76, 0.82, 0.38, 0.0, 0.32, 0.66, 0.0, 0.23],
+    f'% Product {inEnzyme2}': [0.54, 0.66, 0.34, 0.0, 0.26, 0.61, 0.0, 0.22],
     f'Activity Z {inEnzyme2}': inEmptyList,
-    f'% Product Rank {inEnzyme2}': inEmptyList,
-    f'% St Dev {inEnzyme2}': [0.1, 0.09, 0.02, 0, 0.06, 0.09, 0, 0.05],
-    f'Predicted {inEnzyme2}': [0.976, 1.0, 0.035, 0.005, 0.053, 0.362, 0.006, 0.01],
-    f'Pred Z {inEnzyme2}': inEmptyList,
-    # f'Predicted ln {inEnzyme2}': inEmptyList,
+    f'Activity Rank {inEnzyme2}': inEmptyList,
+    f'% St Dev {inEnzyme2}': [0.01, 0.058, 0.025, 0.0, 0.027, 0.044, 0.0, 0.033],
+    f'Predicted {inEnzyme2}': [0.572, 1.0, 0.015, 0.01, 0.038, 0.51, 0.034, 0.066],
+    f'Predicted Z {inEnzyme2}': inEmptyList,
     f'Predicted Rank {inEnzyme2}': inEmptyList,
 }
-inPlotTable = True
-inNatLog = False
 
-# Z-Scores
+# Input: Table
+inPlotTable = True
+inTableEnz = inEnzyme
+inTableCols = [f'% Product {inTableEnz}',
+               f'Activity Z {inTableEnz}', f'Activity Rank {inTableEnz}',
+               f'Predicted Z {inTableEnz}', f'Predicted Rank {inTableEnz}']
+
+# Input: Z-Scores
 inCalcZScores = [ # Format: (x, y), use "x" values to calc the Z-Score saved in "y"
-    (f'% Product {inEnzyme}', f'Activity Z {inEnzyme}'),
-    (f'Predicted {inEnzyme}', f'Pred Z {inEnzyme}'),
     (f'% Product {inEnzyme2}', f'Activity Z {inEnzyme2}'),
-    (f'Predicted {inEnzyme2}', f'Pred Z {inEnzyme2}')
+    (f'Predicted {inEnzyme2}', f'Predicted Z {inEnzyme2}'),
+    (f'% Product {inEnzyme}', f'Activity Z {inEnzyme}'),
+    (f'Predicted {inEnzyme}', f'Predicted Z {inEnzyme}')
 ]
 
-# Scatter plot
-inPlotBoth = True
-inScatter = [f'Activity {inEnzyme2}', f'Activity Z {inEnzyme2}',
-             f'Pred {inEnzyme2}', f'Pred Z {inEnzyme2}']
+# Input: Scatter Plot
+inPlotBoth = True # Add the secondary enzyme to the scatter plot
 
-# Save Figure
+# Input: Save Figure
 inSavePath = ''
 inFigResolution = 600
 
@@ -100,47 +104,68 @@ def convertNum(data, key):
 
 
 def plotTable():
-    data = pd.DataFrame(0.0, index=range(len(inData['Substrates'])), columns=[])
+    data = {}
     for col in inData.keys():
+        # print(f'\nCOL: {col}')
         if '% st dev' in col.lower():
             continue
-        if '% product' in col.lower() and 'rank' not in col.lower():
+        elif 'substrates' in col.lower():
+            data[col] = inData[col]
+        elif '% product' in col.lower() and 'rank' not in col.lower():
             data[col] = convertNum(inData, col)
         elif ('predicted' in col.lower() and
               'rank' not in col.lower() and
               'ln' not in col.lower()):
-            val = []
+            print(f'Col: {col}')
+            key = col.replace('Rank', 'Z')
+            vals = []
             for v in inData[col]:
-                if inSigFigs == 0 or not inSigFigs:
-                    val.append(int(v * 100))
-                else:
-                    val.append((v * 100).round(inSigFigs))
-            data[col] = val
+
+                vals.append(round(v, inRoundVal))
+                # if inSigFigs == 0 or not inSigFigs:
+                #     vals.append(int(v * 100))
+                # else:
+                #     vals.append(str(round((v * 100), 0)))
+            # data[col] = [str(v) for v in vals]
+            data[col] = vals
+            print(f'* Pred: {data[col]}\n')
         elif 'rank' in col.lower():
-            key = col.replace(' Rank', '')
-            data.loc[:, col] = (
-                pd.Series(inData[key]).rank(ascending=False, method='min').astype(int)
-            )
-        elif 'substrates' in col.lower():
-            data[col] = inData[col]
+            # print(f'* Rank: {col}')
+            key = col.replace('Rank', 'Z')
+            # print(f'* Rank: {col}, {key}')
+            # print(inData[key])
+            rank = list(pd.Series(inData[key]).rank(ascending=False, method='min'))
+            l = []
+            for val in rank:
+                l.append(f'{str(int(val))}')
+            data[col] = l
         else:
-            print(f'* {col}')
-            subs = inData['Substrates']
-            print(inData[col])
-
+            x = []
             for i in range(len(inData[col])):
-                data.loc[i, col] = round(inData[col][i], 3)
-            print(f'Data:\n{data.loc[:, col]}\n')
+                x.append(round(inData[col][i], inRoundVal))
+            data[col] = x
+            # print(f'* Data: {data[col]}')
+    # print()
+    # for k, v in data.items():
+    #     print(f'* {k}\n{v}\n')
+    # print()
 
-    table = pd.DataFrame.from_dict(data)
-    table.drop(f'Predicted {inEnzyme}', axis=1, inplace=True)
-    table.drop(f'Predicted {inEnzyme2}', axis=1, inplace=True)
+    # Create table
+    if 'Substrates' not in inTableCols:
+        inTableCols.insert(0, 'Substrates')
+    table = pd.DataFrame('', index=[], columns=inTableCols)
+    for col in table.columns:
+        table.loc[:, col] = data[col]
+
+    # table.drop(f'Predicted {inEnzyme2}', axis=1, inplace=True)
+    # table.drop(f'Predicted {inEnzyme}', axis=1, inplace=True)
     print(f'Table:\n{table}\n')
+    # sys.exit()
 
     # Make figure
     h = (len(table.index) / 2) - 1
     w = len(table.columns) * 2
-    fig, ax = plt.subplots(figsize=(w, h))
+    fig, ax = plt.subplots(figsize=(1.2*w, 1.2*h))
     ax.axis('off')  # hide axes
     tbl = plt.table(cellText=table.values,
                     colLabels=table.columns,
@@ -155,7 +180,7 @@ def plotTable():
         tbl[0, i].set_text_props(weight='bold')
 
     tbl.auto_set_font_size(False)
-    tbl.set_fontsize(12)
+    tbl.set_fontsize(16)
 
     for (row, col), cell in tbl.get_celld().items():
         text = cell.get_text()
@@ -174,19 +199,18 @@ def plotTable():
         print(f'The figure was not saved\n')
 
 
-def zScore(values, tag):
-    print(f'Calculate Z Score: {tag}')
+def zScore(tag):
+    # print(f'Calculate Z Score: {tag}')
+    values = inData[tag]
     avg = np.mean(values)
     stdev = np.std(values)
     z = []
     for x in values:
         z.append((x - avg) / stdev)
-
-    rVal = 3
     for i in range(len(values)):
-        x, y = round(float(values[i]),rVal), round(float(z[i]),rVal)
-        print(f'* Value: {x}, Z Score: {y}')
-    print()
+        x, y = round(float(values[i]), inRoundVal), round(float(z[i]), inRoundVal)
+        # print(f'* Value: {x}, Z Score: {y}')
+    # print()
     return z
 
 
@@ -195,162 +219,96 @@ def ln(vals):
     for val in vals:
         l = np.log(val)
         x.append(l)
-        print(f'*: ln({round(val,3)}) -> {round(l,3)}')
+        print(f'*: ln({round(val, inRoundVal)}) -> {round(l, inRoundVal)}')
     print()
     return x
 
 
-from scipy.optimize import curve_fit
-
-def exp_func(x, a, b, c):
+def fnExp(x, a, b, c):
     return a * np.exp(b * x) + c
 
-def fitExp(x, y):
+
+def fitData(x, y):
     # Fit the curve
-    popt, pcov = curve_fit(exp_func, x, y, p0=[1, 1, 0], maxfev=10000)
+    popt, pcov = curve_fit(fnExp, x, y, p0=[1, 1, 0], maxfev=10000)
     a, b, c = popt
 
     # Generate smooth curve for plotting
-    x_fit = np.linspace(x.min(), x.max(), 300)
-    y_fit = exp_func(x_fit, *popt)
+    xFit = np.linspace(x.min(), x.max(), 300)
+    yFit = fnExp(xFit, *popt)
 
     # R² for the exponential fit
-    y_pred = exp_func(x, *popt)
-    ss_res = np.sum((y - y_pred) ** 2)
+    yPred = fnExp(x, *popt)
+    ss_res = np.sum((y - yPred) ** 2)
     ss_tot = np.sum((y - np.mean(y)) ** 2)
-    exp_r2 = 1 - (ss_res / ss_tot)
-    return x_fit, y_fit, exp_r2
+    r2 = 1 - (ss_res / ss_tot)
+
+    return xFit, yFit, r2
 
 
 # ========================================================================================
-
-
 normalizeData()
-# z = True
-# if z:
-#     def alt(n, m):
-#         q = (n + m) * (100 / m)
-#         return round(q, 5)
-#
-#     print(f'Values:\n{inData[f'Predicted {inEnzyme}']}')
-#     print('ln()')
-#     x = ln(inData[f'Predicted {inEnzyme}'])
-#     # x = zScore(x, inEnzyme)
-#     for i in range(len(x)):
-#         n = x[i]
-#         m = 4.605
-#         x[i] = alt(n, m)
-#     inData[f'Predicted ln {inEnzyme}'] = x
-#     # x = zScore(inData[f'Predicted {inEnzyme}'], inEnzyme)
-#     x = ln(inData[f'Predicted {inEnzyme2}'])
-#     for i in range(len(x)):
-#         n = x[i]
-#         m = 5.298
-#         x[i] = alt(n, m)
-#     inData[f'Predicted ln {inEnzyme2}'] = x
-# else:
-#     inData[f'Predicted ln {inEnzyme}'] = ln(inData[f'Predicted {inEnzyme}'])
-#     inData[f'Predicted ln {inEnzyme2}'] = ln(inData[f'Predicted {inEnzyme2}'])
-
 
 # Calculate Z-Scores
 for tags in inCalcZScores:
-    inData[tags[1]] = zScore(inData[tags[0]], tags[0])
+    inData[tags[1]] = zScore(tags[0])
     # inData[tags[1]] = ln(inData[tags[0]])
 
-# if inPlotTable:
-#     plotTable()
+if inPlotTable:
+    plotTable()
+# sys.exit()
 
 # Evaluate the natural log
 if inNatLog:
-    inData[f'Predicted {inEnzyme}'] = ln(inData[f'Predicted {inEnzyme}'])
     inData[f'Predicted {inEnzyme2}'] = ln(inData[f'Predicted {inEnzyme2}'])
+    inData[f'Predicted {inEnzyme}'] = ln(inData[f'Predicted {inEnzyme}'])
+
 
 # ========================================================================================
 # Evaluate data
-data = pd.DataFrame(0.0, index=inData['Substrates'],
-                    columns=inScatter)
-data[f'Activity {inEnzyme2}'] = inData[f'% Product {inEnzyme2}']
-data[f'Activity Z {inEnzyme2}'] = inData[f'Activity Z {inEnzyme2}']
-data[f'Pred {inEnzyme2}'] = inData[f'Predicted {inEnzyme2}']
-data[f'Pred Z {inEnzyme2}'] = inData[f'Pred Z {inEnzyme2}']
-
-actR2 = round(r2_score(data[f'Activity {inEnzyme2}'], data[f'Pred {inEnzyme2}']), 3)
-actZR2 = round(r2_score(data[f'Activity Z {inEnzyme2}'], data[f'Pred Z {inEnzyme2}']), 3)
-print(f'\nZ Scores: {inEnzyme2}\n{data}\n')
+data = pd.DataFrame(0.0, index=inData['Substrates'], columns=[])
+data[f'Activity {inEnzyme}'] = inData[f'% Product {inEnzyme}']
+data[f'Activity Z {inEnzyme}'] = inData[f'Activity Z {inEnzyme}']
+data[f'Pred {inEnzyme}'] = inData[f'Predicted {inEnzyme}']
+data[f'Predicted Z {inEnzyme}'] = inData[f'Predicted Z {inEnzyme}']
+actR2 = round(r2_score(data[f'Activity {inEnzyme}'], data[f'Pred {inEnzyme}']), inRoundVal)
+actZR2 = round(r2_score(data[f'Activity Z {inEnzyme}'], data[f'Predicted Z {inEnzyme}']), inRoundVal)
+print(f'{data}\n\n')
 
 
-# Evaluate a second set
-if inPlotBoth:
-    data[f'Activity {inEnzyme}'] = inData[f'% Product {inEnzyme}']
-    data[f'Activity Z {inEnzyme}'] = inData[f'Activity Z {inEnzyme}']
-    data[f'Pred {inEnzyme}'] = inData[f'Predicted {inEnzyme}']
-    data[f'Pred Z {inEnzyme}'] = inData[f'Pred Z {inEnzyme}']
-    print(f'\nZ Scores: {inEnzyme}\n{data}\n')
-    actEnzR2 = round(
-        r2_score(data[f'Activity {inEnzyme}'], data[f'Pred {inEnzyme}']), 3
-    )
-    actEnzZR2 = round(
-        r2_score(data[f'Activity Z {inEnzyme}'], data[f'Pred Z {inEnzyme}']), 3
-    )
+# Evaluate the secondary set
+data2 = pd.DataFrame(0.0, index=inData['Substrates'], columns=[])
+data2[f'Activity {inEnzyme2}'] = inData[f'% Product {inEnzyme2}']
+data2[f'Activity Z {inEnzyme2}'] = inData[f'Activity Z {inEnzyme2}']
+data2[f'Pred {inEnzyme2}'] = inData[f'Predicted {inEnzyme2}']
+data2[f'Predicted Z {inEnzyme2}'] = inData[f'Predicted Z {inEnzyme2}']
+print(f'{data2}\n\n')
+actEnzR2 = round(
+    r2_score(data2[f'Activity {inEnzyme2}'], data2[f'Pred {inEnzyme2}']), inRoundVal
+)
+actEnzZR2 = round(
+    r2_score(data2[f'Activity Z {inEnzyme2}'], data2[f'Predicted Z {inEnzyme2}']), inRoundVal
+)
 
 
 # Plot data
+c1, c2, = '#A800FF', '#BF5700'
 inTitleSize = 16
 inLabelSize = 14
-c1, c2, = '#2E9418', '#BF5700'
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
-# data.plot(
-#     x=f'Activity {inEnzyme2}', y=f'Pred {inEnzyme2}', ax=ax1, marker='o',
-#     linestyle='none', color=c2, legend=f'R² = {actR2}'
-# )
-# data.plot(
-#     x=f'Activity Z {inEnzyme2}', y=f'Pred Z {inEnzyme2}', ax=ax2, marker='s',
-#     linestyle='none', color=c2, legend=f'R² = {actZR2}'
-# )
-x_fit, y_fit, fitCurve = fitExp(x=data[f'Activity {inEnzyme2}'].values,
-                            y=data[f'Pred {inEnzyme2}'].values)
+x, y = f'Activity {inEnzyme}', f'Pred {inEnzyme}'
+x_fit, y_fit, fitCurve = fitData(x=data[x].values, y=data[y].values)
 data.plot(
-    x=f'Activity {inEnzyme2}', y=f'Pred {inEnzyme2}', ax=ax1, marker='o',
-    linestyle='none', color=c2, label=f'R² = {fitCurve:.3f}'
+    x=x, y=y, ax=ax1, marker='^',linestyle='none', color=c2, label=f'R² = {fitCurve:.3f}'
 )
 ax1.plot(x_fit, y_fit, color=c2, linestyle='-', linewidth=1.5)
 
-x_fit2, y_fit2, fitCurve2 = fitExp(x=data[f'Activity Z {inEnzyme2}'].values,
-                                   y=data[f'Pred Z {inEnzyme2}'].values)
+x, y = f'Activity Z {inEnzyme}', f'Predicted Z {inEnzyme}'
+x_fit2, y_fit2, fitCurve2 = fitData(x=data[x].values, y=data[y].values)
 data.plot(
-    x=f'Activity Z {inEnzyme2}', y=f'Pred Z {inEnzyme2}', ax=ax2, marker='s',
-    linestyle='none', color=c2, legend=f'R² = {fitCurve2:.3f}'
+    x=x, y=y, ax=ax2, marker='D', linestyle='none', color=c2, legend=f'R² = {fitCurve2:.3f}'
 )
 ax2.plot(x_fit2, y_fit2, color=c2, linestyle='-', linewidth=1.5)
-
-
-if inPlotBoth:
-    # data.plot(
-    #     x=f'Activity {inEnzyme}', y=f'Pred {inEnzyme}', ax=ax1, marker='D',
-    #     linestyle='none', color=c1, legend=f'R² = {actEnzR2}'
-    # )
-    # data.plot(
-    #     x=f'Activity Z {inEnzyme}', y=f'Pred Z {inEnzyme}', ax=ax2, marker='*',
-    #     linestyle='none', color=c1, legend=f'R² = {actEnzZR2}'
-    # )
-    x_fit3, y_fit3, fitCurve3 = fitExp(x=data[f'Activity {inEnzyme}'].values,
-                                       y=data[f'Pred {inEnzyme}'].values)
-    data.plot(
-        x=f'Activity Z {inEnzyme}', y=f'Pred Z {inEnzyme}', ax=ax1, marker='s',
-        linestyle='none', color=c1, legend=f'R² = {fitCurve3:.3f}'
-    )
-    ax1.plot(x_fit2, y_fit2, color=c1, linestyle='--', linewidth=1.5)
-
-    x_fit4, y_fit4, fitCurve4 = fitExp(x=data[f'Activity Z {inEnzyme}'].values,
-                                       y=data[f'Pred Z {inEnzyme}'].values)
-    data.plot(
-        x=f'Activity Z {inEnzyme}', y=f'Pred Z {inEnzyme}', ax=ax2, marker='s',
-        linestyle='none', color=c1, legend=f'R² = {fitCurve4:.3f}'
-    )
-    ax2.plot(x_fit4, y_fit4, color=c1, linestyle='--', linewidth=1.5)
-
-
 fig.suptitle(f'Enzyme Activity', fontsize=inTitleSize,
              fontweight='bold', va='top')
 
@@ -370,42 +328,40 @@ ax2.tick_params(axis='y', labelsize=inLabelSize-2)
 
 # Legend
 if inPlotBoth:
-    # ax1.legend(prop=FontProperties(size=10, weight='bold'), handles=[Line2D(
-    #     [], [], linestyle='None', marker='None',
-    #     label=f'R² {inEnzyme}  = {actEnzR2:.3f}\nR² {inEnzyme2} = {actR2:.3f}')],
-    #            handletextpad=0, handlelength=0
-    # )
-    # ax2.legend(prop=FontProperties(size=10, weight='bold'), handles=[Line2D(
-    #     [], [], linestyle='None', marker='None',
-    #     label=f'R² {inEnzyme}  = {actEnzZR2:.3f}\nR² {inEnzyme2} = {actZR2:.3f}')],
-    #            handletextpad=0, handlelength=0
-    # )
+    x, y = f'Activity {inEnzyme2}', f'Pred {inEnzyme2}'
+    x_fit3, y_fit3, fitCurve3 = fitData(x=data2[x].values, y=data2[y].values)
+    data2.plot(
+        x=x, y=y, ax=ax1, marker='*', linestyle='none', color=c1,
+        legend=f'R² = {fitCurve3:.3f}'
+    )
+    ax1.plot(x_fit3, y_fit3, color=c1, linestyle='--', linewidth=1.5)
+
+    x, y = f'Activity Z {inEnzyme2}', f'Predicted Z {inEnzyme2}'
+    x_fit4, y_fit4, fitCurve4 = fitData(x=data2[x].values, y=data2[y].values)
+    data2.plot(
+        x=x, y=y, ax=ax2, marker='o', linestyle='none', color=c1,
+        legend=f'R² = {fitCurve4:.3f}'
+    )
+    ax2.plot(x_fit4, y_fit4, color=c1, linestyle='--', linewidth=1.5)
+
     ax1.legend(prop=FontProperties(size=10, weight='bold'), handles=[Line2D(
         [], [], linestyle='None', marker='None',
-        label=f'R² {inEnzyme}  = {fitCurve3:.3f}\nR² {inEnzyme2} = {fitCurve:.3f}')],
+        label=f'R² {inEnzyme2}  = {fitCurve3:.3f}\nR² {inEnzyme} = {fitCurve:.3f}')],
                handletextpad=0, handlelength=0
                )
     ax2.legend(prop=FontProperties(size=10, weight='bold'), handles=[Line2D(
         [], [], linestyle='None', marker='None',
-        label=f'R² {inEnzyme}  = {fitCurve4:.3f}\nR² {inEnzyme2} = {fitCurve2:.3f}')],
+        label=f'R² {inEnzyme2}  = {fitCurve4:.3f}\nR² {inEnzyme} = {fitCurve2:.3f}')],
                handletextpad=0, handlelength=0
                )
 else:
-    # ax1.legend(prop=FontProperties(size=10, weight='bold'), handles=[Line2D(
-    #     [], [], linestyle='None', marker='None',
-    #     label=f'R² {inEnzyme2} = {actR2:.3f}')], handletextpad=0, handlelength=0
-    # )
-    # ax2.legend(prop=FontProperties(size=10, weight='bold'), handles=[Line2D(
-    #     [], [], linestyle='None', marker='None',
-    #     label=f'R² {inEnzyme2} = {actZR2:.3f}')], handletextpad=0, handlelength=0
-    # )
     ax1.legend(prop=FontProperties(size=10, weight='bold'), handles=[Line2D(
         [], [], linestyle='None', marker='None',
-        label=f'R² {inEnzyme2} = {fitCurve:.3f}')], handletextpad=0, handlelength=0
+        label=f'R² {inEnzyme} = {fitCurve:.3f}')], handletextpad=0, handlelength=0
                )
     ax2.legend(prop=FontProperties(size=10, weight='bold'), handles=[Line2D(
         [], [], linestyle='None', marker='None',
-        label=f'R² {inEnzyme2} = {fitCurve2:.3f}')], handletextpad=0, handlelength=0
+        label=f'R² {inEnzyme} = {fitCurve2:.3f}')], handletextpad=0, handlelength=0
                )
 
 plt.tight_layout()
